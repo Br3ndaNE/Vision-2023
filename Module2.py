f322b2e9507b40bfaa97728f826a3a0562122acc
Br3ndaNE <77840520+Br3ndaNE@users.noreply.github.com>
2023-02-20 15:13:03 +0100

    Progress Module 2

--- Module2.py
+++ Module2.py
@@ -3,12 +3,11 @@ from skimage.viewer import ImageViewer
 import numpy as np
 import scipy
 from scipy import ndimage
+import matplotlib.pyplot as plt
 
 image = data.camera()
-viewer = ImageViewer(image)
-viewer.show()
 
-filter = filters.gaussian(image, 0.1)
+filter = filters.gaussian(image, 0.8 )
 
 mask1=[[-1/9,0,1/9],[-1/9,0,1/9],[-1/9,0,1/9]]
 mask2=[[-1/9,-1/9,-1/9], [0,0,0], [1/9,1/9,1/9]]
@@ -16,7 +15,20 @@ mask2=[[-1/9,-1/9,-1/9], [0,0,0], [1/9,1/9,1/9]]
 newimage0=scipy.ndimage.convolve(filter, mask1)
 newimage1=scipy.ndimage.convolve(filter, mask2)
 
-dogwater_ass = np.sqrt(np.square(newimage0) + np.square(newimage1))
+dogwater_ass = np.sqrt( np.square( newimage0 ) + np.square( newimage1 ) )
 
-viewer = ImageViewer(dogwater_ass)
-viewer.show()
+fig = plt.figure(figsize=(16, 9))
+
+fig.add_subplot(1, 4, 1)
+plt.imshow(filter)
+
+fig.add_subplot(1, 4, 2)
+plt.imshow(newimage0)
+
+fig.add_subplot(1, 4, 3)
+plt.imshow(newimage1)
+
+fig.add_subplot(1, 4, 4)
+plt.imshow(dogwater_ass)
+
+plt.show()
